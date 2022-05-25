c0efb502e333e69dc8e4309b699d570595dd83c7
JongIn Yoon <bellpp93@gmail.com>
2022-05-25 17:08:04 +0900

    0525_django_mysite

--- pybo/models.py
+++ pybo/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# 질문 모델 작성
 class Question(models.Model):
     subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.subject
+
+# 답변 모델 작성
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
