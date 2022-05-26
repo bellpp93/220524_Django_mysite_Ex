53baaec6f73edd3c96cec2bf20cd7213b5ebaf79
JongIn Yoon <bellpp93@gmail.com>
2022-05-26 17:16:13 +0900

    0526_django_mysite

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('안녕하세요 pybo에 오신 것을 환영합니다.')
+    """
+        pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')  # -붙으면 내림차순 없으면 오름차순
+    context = {'question_list': question_list}  # 딕셔너리의 {키 : 값}
+    return render(request, 'pybo/question_list.html', context)  # 인자 값 3개
+
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)  # get_object_or_404 추가 후 아래처럼 수정
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
