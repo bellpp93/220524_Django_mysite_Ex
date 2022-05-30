8af18c2dccfb65fe17e4fdefa09022460b403502
JongIn Yoon <bellpp93@gmail.com>
2022-05-30 18:01:42 +0900

    0530_Django 부트스트랩, 템플릿 상속, 폼

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm
 
 # Create your views here.
 def index(request):
@@ -26,3 +27,19 @@ def answer_create(request, question_id):
     question.answer_set.create(content=request.POST.get('content'),
                                create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+def question_create(request):
+    """
+        pybo 질문 등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():  # 폼이 유효하다면
+            question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴받는다.
+            question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정한다.
+            question.save()  # 데이터를 실제로 저장한다.
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
